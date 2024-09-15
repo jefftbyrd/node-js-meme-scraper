f3ed695339796ced5077dc27fffa14f4d1d28c08
Jeff T Byrd <jeff.t.byrd@gmail.com>
2024-09-15 18:28:48 +0200

    Finalize 6. Maybe this is the one!

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
+import fs from 'node:fs';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import fs from 'fs';
-
-const memeImages = [];
-const fileNames = [];
 
 if (!fs.existsSync('memes')) {
   fs.mkdirSync('memes');
@@ -13,27 +10,22 @@ async function scrapeMemes() {
   try {
     const url = `https://memegen-link-examples-upleveled.netlify.app/`;
     const { data } = await axios.get(url);
-
     const $ = cheerio.load(data);
 
-    $('#images div a').each((i, elem) => {
+    $('#images div a').each(async (i, elem) => {
       const memeImage = $(elem).find('img').attr('src');
-      let fileName = i++;
+      const fileName = i++;
 
-      if (memeImage && fileNames.length < 10) {
-        axios
-          .get(memeImage, { responseType: 'arraybuffer' })
-          .then((response) => {
-            if (fileName <= 8) {
-              fs.writeFileSync(`memes/0${fileName + 1}.jpg`, response.data);
-            } else {
-              fs.writeFileSync(`memes/${fileName + 1}.jpg`, response.data);
-            }
-          });
-
-        memeImages.push(memeImage);
-        fileNames.push(fileName);
-      }
+      await axios
+        .get(memeImage, { responseType: 'arraybuffer' })
+        .then((response) => {
+          if (fileName <= 8) {
+            fs.writeFileSync(`memes/0${fileName + 1}.jpg`, response.data);
+          } else if (fileName === 9) {
+            fs.writeFileSync(`memes/${fileName + 1}.jpg`, response.data);
+          } else {
+          }
+        });
     });
   } catch (error) {
     console.log(error);
